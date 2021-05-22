bf967ac2896fced31c553a731a262820fd0a0d2f
m <mikeallison79@gmail.com>
2021-05-22 14:57:28 -0400

    Changed merchant input element to a dropdown

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,18 +1,35 @@
 import { Transaction } from './transaction.js';
 
 class App {
-  static renderHook = document.getElementById('app');
-
-  static init() {
-    this.merchantInput = document.getElementById('merchant-input');
+  constructor() {
+    this.merchantSelect = document.getElementById('merchant-select');
     this.amountInput = document.getElementById('amount-input');
     this.cardBtns = document.querySelectorAll('.card-btn');
 
+    this.merchantList = [
+      { id: 1, name: 'Solid State' },
+      { id: 2, name: 'Grimm' },
+      { id: 3, name: 'Sotto Le Stelle' }
+    ];
+  }
+
+  init() {
+    // Initialize merchant dropdown
+    this.merchantList.sort().forEach(merchant => {
+      const optionEl = document.createElement('option');
+      optionEl.setAttribute('value', merchant.id);
+      optionEl.innerText = merchant.name;
+      this.merchantSelect.appendChild(optionEl);
+    });
+
+    $('select.dropdown').dropdown();
+
+    // Initialize card number submit buttons
     this.cardBtns.forEach(btn => {
       btn.addEventListener('click', e => {
         const transaction = new Transaction(
           e.currentTarget.dataset.cardId,
-          this.merchantInput.value,
+          this.merchantSelect.value,
           this.amountInput.value
         );
         // Check values
@@ -27,4 +44,5 @@ class App {
   }
 }
 
-App.init();
+const app = new App();
+app.init();
