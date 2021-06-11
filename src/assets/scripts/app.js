2e285cc7696273bfe87cbc411939ade77ccc0de1
m <mikeallison79@gmail.com>
2021-06-10 22:08:37 -0400

    Added sort to merchant dropdown

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -71,6 +71,18 @@ class App {
         });
       })
       .then(() => {
+        this.merchantList.sort((a, b) => {
+          var nameA = a.name.toUpperCase();
+          var nameB = b.name.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+
         this.merchantList.forEach(merchant => {
           const optionEl = document.createElement('option');
           optionEl.setAttribute('value', merchant.name);
